f835512ba4b5573fcf7dc63d898129c059eaee3b
SF4NU <mariusmarius200121@gmail.com>
2024-03-13 23:48:05 +0100

    collegato il server a postgresql in locale e aggiunto il get e il post

--- main.go
+++ main.go
@@ -2,38 +2,69 @@ package main
 
 import (
 	"net/http"
-	"fmt"
+
 	"github.com/labstack/echo/v4"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-func getUser(c echo.Context) error {
-	id := c.Param("id")
-	return c.String(http.StatusOK, id)
-}
+// func getUser(c echo.Context) error {
+// 	id := c.Param("id")
+// 	return c.String(http.StatusOK, id)
+// }
 
-type User struct {
-	Name string `json:"name"`
-	Email string `json:"email"`
+// type User struct {
+// 	Name string `json:"name"`
+// 	Email string `json:"email"`
+// }
+
+type Task struct {
+	ID          uint   `gorm:"primaryKey"`
+	Description string `json:"description"`
+	Completed   bool   `json:"completed"`
 }
 
 func main() {
+	dsn := "host=localhost user=postgres password=123 dbname=todo_db port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("db not connected")
+	}
+
+	db.AutoMigrate(&Task{})
+
 	e := echo.New()
 
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+	e.GET("/tasks", func(c echo.Context) error {
+		var tasks []Task
+		db.Find(&tasks)
+		return c.JSON(http.StatusOK, tasks)
 	})
 
-	e.POST("/users", func(c echo.Context) error {
-		u := new(User)
-		if err := c.Bind(u); err != nil {
-			return err
+	e.POST("/tasks", func(c echo.Context) error {
+		var task Task
+		if err := c.Bind(&task); err != nil {
+				return err
 		}
+		db.Create(&task)
+		return c.JSON(http.StatusCreated, task)
+})
 
-		fmt.Println(u.Email)
-		return c.JSON(http.StatusCreated, u)
-	})
+	// e.GET("/", func(c echo.Context) error {
+	// 	return c.String(http.StatusOK, "Hello, World!")
+	// })
 
-	e.GET("/users/:id", getUser)
+	// e.POST("/users", func(c echo.Context) error {
+	// 	u := new(User)
+	// 	if err := c.Bind(u); err != nil {
+	// 		return err
+	// 	}
+
+	// 	fmt.Println(u.Email)
+	// 	return c.JSON(http.StatusCreated, u)
+	// })
+
+	// e.GET("/users/:id", getUser)
 	// e.PUT("/users/:id", updateUser)
 	// e.DELETE("/users/:id", deleteUser)
 
